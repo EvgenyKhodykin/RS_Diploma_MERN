36d88353123c8331e1eecfd2ce8b4627849f21ea
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-08-22 17:50:16 +0500

    regForm sex

--- client/src/components/forms/RegisterForm.jsx
+++ client/src/components/forms/RegisterForm.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
-import { Box, Button, Paper, TextField, Typography } from '@mui/material'
+import {
+    Box,
+    Button,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Paper,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography
+} from '@mui/material'
 import { Link } from 'react-router-dom'
 
 function RegisterForm() {
@@ -23,6 +34,32 @@ function RegisterForm() {
                     autoComplete='current-password'
                     sx={{ width: '100%', my: 1 }}
                 ></TextField>
+                <TextField
+                    required
+                    label='Имя'
+                    type='name'
+                    autoComplete='current-name'
+                    sx={{ width: '100%', my: 1 }}
+                ></TextField>
+                <FormControl sx={{ ml: 1 }}>
+                    <FormLabel id='demo-radio-buttons-group-label'>Пол</FormLabel>
+                    <RadioGroup
+                        aria-labelledby='demo-radio-buttons-group-label'
+                        defaultValue='male'
+                        name='radio-buttons-group'
+                    >
+                        <FormControlLabel
+                            value='male'
+                            control={<Radio />}
+                            label='Мужской'
+                        />
+                        <FormControlLabel
+                            value='female'
+                            control={<Radio />}
+                            label='Женский'
+                        />
+                    </RadioGroup>
+                </FormControl>
                 <Button variant='contained' size='large' sx={{ width: '100%', my: 2 }}>
                     Зарегистрироваться
                 </Button>
