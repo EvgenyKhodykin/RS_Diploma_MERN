7c867531742241b29c4478ca24e57b22619ac399
EvgenyKhodykin <dr.khodykin@gmail.com>
2023-08-30 21:07:54 +0500

    register bag

--- client/src/components/forms/RegisterForm.jsx
+++ client/src/components/forms/RegisterForm.jsx
@@ -26,7 +26,7 @@ function RegisterForm() {
     const [data, setData] = useState({
         email: '',
         password: '',
-        sex: '',
+        sex: 'Мужской',
         name: ''
     })
     const [showPassword, setShowPassword] = useState(false)
